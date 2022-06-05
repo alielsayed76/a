fb938b279f39be7184cc711a601ad1e385c69433
alielsayed76 <98778110+alielsayed76@users.noreply.github.com>
2022-06-05 18:21:34 +0200

    Update filters.py

--- driver/filters.py
+++ driver/filters.py
@@ -1,4 +1,3 @@
-import re
 from pyrogram import filters
 from typing import List, Union
 from config import COMMAND_PREFIXES
@@ -11,23 +10,4 @@ other_filters2 = (
 
 
 def command(commands: Union[str, List[str]]):
-    return filters.command(commands, COMMAND_PREFIXES)
-
-
-def commandpro(commands: Union[str, List[str]], prefixes: Union[str, List[str]] = "ح ع ر ب م", case_sensitive: bool = False):
-    """Filter commands, i.e.: text messages starting with "/" or any other custom prefix.
-    Parameters:
-        commands (``str`` | ``list``):
-            The command or list of commands as string the filter should look for.
-            Examples: "start", ["start", "help", "settings"]. When a message text containing
-            a command arrives, the command itself and its arguments will be stored in the *command*
-            field of the :obj:`~pyrogram.types.Message`.
-        prefixes (``str`` | ``list``, *optional*):
-            A prefix or a list of prefixes as string the filter should look for.
-            Defaults to "/" (slash). Examples: ".", "!", ["/", "!", "."], list(".:!").
-            Pass None or "" (empty string) to allow commands with no prefix at all.
-        case_sensitive (``bool``, *optional*):
-            Pass True if you want your command(s) to be case sensitive. Defaults to False.
-            Examples: when True, command="Start" would trigger /Start but not /start.
-    """
-    commandpro_re = re.compile(r"([\"'])(.*?)(?<!\\)\1|(\S+)")
+    return filters.command(commands, None)
