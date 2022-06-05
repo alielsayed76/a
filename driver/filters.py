f6a3a3a1baf8f04efa14a47aa6aecd3c228baf5f
alielsayed76 <98778110+alielsayed76@users.noreply.github.com>
2022-06-05 18:50:35 +0200

    Update filters.py

--- driver/filters.py
+++ driver/filters.py
@@ -9,7 +9,7 @@ other_filters2 = (
 
 
 def command(commands: Union[str, List[str]]):
-    return filters.command(commands,"")
+    return filters.command(commands,None)
 
 
 def commandpro(commands: Union[str, List[str]]):
